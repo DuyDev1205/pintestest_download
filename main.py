d6835a4beb342e8b398012b475a5a35bc9699352
Trần Thanh Duy <DUY.TT22714@sinhvien.hsu.edu.vn>
2024-04-29 02:56:52 +0700

    final code

--- main.py
+++ main.py
@@ -21,40 +21,58 @@ def clear_chrome_profile_cache(profile_path):
             print(f"Deleted cache at: {path}")
         else:
             print(f"Cache path not found: {path}")
+def wait_for_xpath_single(driver, xpath):
+    return WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, xpath))
+    )
+
+def wait_for_xpath_all(driver, xpath):
+    return WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.XPATH, xpath))
+    )
+
 def download_image(image_url, file_name):
     response = requests.get(image_url)
     if response.status_code == 200:
         with open(file_name, 'wb') as f:
             f.write(response.content)
+        return True
+    return False
 
-def wait_for_xpath_single(xpath):
-    return WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, xpath))
-    )
+def final():
+    options = Options()
+    options.add_argument("user-data-dir=D:\\VSCode\\Codeathon\\pintestest_download\\ChromeProfile")
+    num = int(input("Số lượng ảnh cần tải: "))
+    value = input("Nội dung cần tìm kiếm ")
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://www.pinterest.com")
+    tim_kiem = wait_for_xpath_single(driver, "//input[@aria-label='Tìm kiếm']")
+    tim_kiem.click()
+    tim_kiem.send_keys(value)
+    tim_kiem.send_keys(Keys.ENTER)
+    sleep(2)
+    
+    downloaded = 0
+    images_processed = set()
 
-def wait_for_xpath_all(xpath):
-    return WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.XPATH, xpath))
-    )
+    while downloaded < num:
+        images = wait_for_xpath_all(driver, '//img[@class="hCL kVc L4E MIw"]')
+        for img in images:
+            src = img.get_attribute('src')
+            if src and src not in images_processed:
+                file_name = os.path.join(os.getcwd(), f'images/image_{downloaded + 1}.jpg')
+                if download_image(src, file_name):
+                    downloaded += 1
+                    images_processed.add(src)
+                    if downloaded >= num:
+                        break
+        if downloaded < num:  # Chỉ cuộn trang khi cần thêm ảnh
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            sleep(3)  # Đợi ảnh mới tải về
 
+    driver.quit()
 
-options = Options()
-options.add_argument("user-data-dir=D:\\VSCode\\Codeathon\\pintestest_download\\ChromeProfile")
-num = int(input("Số lượng ảnh cần tải: "))
-value = input("Nội dung cần tìm kiếm ")
-driver = webdriver.Chrome(options=options)
-driver.get("https://www.pinterest.com")
-tim_kiem = wait_for_xpath_single("//input[@aria-label='Tìm kiếm']")
-tim_kiem.click()
-tim_kiem.send_keys(value)
-tim_kiem.send_keys(Keys.ENTER)
-sleep(2)
-images = wait_for_xpath_all('//img[@class="hCL kVc L4E MIw"]')
-for idx, img in enumerate(images[1:num+1],start=1):
-    src = img.get_attribute('src')
-    if src:
-        file_name = os.path.join(os.getcwd(), f'images/image_{idx}.jpg')
-        download_image(src, file_name)
-profile_path = 'D:\\VSCode\\Codeathon\\pintestest_download\\ChromeProfile\\Default'
-clear_chrome_profile_cache(profile_path)        
-driver.quit()
+if __name__ == "__main__":
+    profile_path = 'D:\\VSCode\\Codeathon\\pintestest_download\\ChromeProfile\\Default'
+    clear_chrome_profile_cache(profile_path)
+    final()
