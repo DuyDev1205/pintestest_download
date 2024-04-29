0baef7d637087863d9545ecd7176d85124bf79a4
Trần Thanh Duy <DUY.TT22714@sinhvien.hsu.edu.vn>
2024-04-30 01:28:20 +0700

    add menu , add show_image,add remove_image

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ import os
 import requests
 from time import sleep
 import shutil
+from PIL import Image
+import pyautogui
+from tabulate import tabulate
+from termcolor import colored
+import json
+class Color:
+    GREEN = '\033[92m'
 def clear_chrome_profile_cache(profile_path):
     cache_paths = [
         os.path.join(profile_path, 'Cache'),  # Thư mục cache chính
@@ -37,7 +44,25 @@ def download_image(image_url, file_name):
             f.write(response.content)
         return True
     return False
-
+def show_image():
+    global path_list
+    for i in path_list:
+        img=Image.open(i)
+        img.show() 
+        pyautogui.hotkey('alt', 'f4')
+def remove_image():
+    for i in path_list:
+        os.remove(i)
+def display_menu():
+    menu_items = [
+        ["1", "Tải ảnh"],
+        ["2", "Xem ảnh"],
+        ["3", "Xóa ảnh"],
+        ["0", "Thoát"],
+    ]
+    headers = ["Lựa chọn", "Chức năng"]
+    menu = tabulate(menu_items, headers, tablefmt="fancy_outline",colalign=("center","center"))
+    print(menu)
 def final():
     options = Options()
     options.add_argument("user-data-dir="+os.path.join(os.getcwd(),f'ChromeProfile'))
@@ -53,6 +78,8 @@ def final():
 
     downloaded = 0
     images_processed = set()
+    global path_list
+    path_list =[]
     while downloaded < num:
         images = wait_for_xpath_all(driver, '//img[@class="hCL kVc L4E MIw"]')
         for img in images[1:num+1]:
@@ -68,10 +95,32 @@ def final():
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             sleep(3)  # Đợi ảnh mới tải về
     driver.quit()
-
+def output():
+    cls=os.system('cls')
+    print(Color.GREEN)
+    display_menu()
+    while True:
+        try:
+                choice = int(input("Nhập lựa chọn: "))
+        except ValueError:
+            print("Vui lòng nhập một số hợp lệ.")
+            continue
+        match choice:
+            case 0:
+                print("Đang thoát chương trình...")
+                break
+            case 1:
+                final()
+            case 2:
+                show_image()
+            case 3:
+                remove_image()
 if __name__ == "__main__":
     path =os.path.join(os.getcwd(),f'ChromeProfile\\Default')
     clear_catche =os.path.join(os.getcwd(),f'ChromeProfile\\Default\\Cache')
     if os.path.exists(clear_catche):
         clear_chrome_profile_cache(path)
-    final()
+    # final()
+    # show_image()
+    # remove_image()
+    output()
